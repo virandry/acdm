9ec370b86ab277c3554d2ea66379f9e32f20d485
Virandry <virandry@gmail.com>
2017-11-08 21:35:58 +0800

    initial commit

---
+++ src/main/java/io/virandry/search/ServletInitializer.java
@@ -0,0 +1,13 @@
+package io.virandry.search;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SearchApplication.class);
+	}
+
+}
